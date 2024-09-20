936e8d4473a8168b13d1128d5f415b7f43d08df7
george <valentegeorge71@gmail.com>
2024-09-20 10:43:36 -0400

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Header } from "./components/Header";
+import  PoemImg  from './assets/poem-img.jpg'
 
 function App() {
 
@@ -33,7 +35,6 @@ function App() {
   
   useEffect(() => {
     getAuthors();
-
   },[])
 
   const getRandomPoetry = async () => { 
@@ -48,11 +49,8 @@ function App() {
   }
 
   const getSelectedLetter = (letter: string) => {
-    if(!authors || authors.length === 0){
-      console.log("authors not loaded yet"); 
-      return; 
-    }
-
+    if(!authors.length) return; 
+    
     let authorsWithLetter = authors.filter((author) => 
       author.startsWith(letter)
   );
@@ -70,9 +68,37 @@ function App() {
   
   return (
     <div className = "font-inter antialiased bg-slate-100 p-2 min-h-screen">
-      <div className="">
-        <h1 className = "text-3xl font-extrabold  my-3">Poetry of The Day</h1>
-        <p className = "text-center  text-gray-500">“We don't read and write poetry because it's cute. We read and write poetry because we are members of the human race. And the human race is filled with passion"</p>
+      <div className="flex gap-2 py-4 px-4 mx-4">
+        <h1 className = "font-bold text-5xl">Dive into Poetry. Explore authors, their works or get a random poetry.</h1>
+        <img className = "w-[40%] rounded-md shadow-2xl" src={PoemImg} alt="" />
+      </div>
+      <div>
+        <h2 className = "text-center my-2 font-semibold">Authors</h2>
+        <ul className = "flex flex-wrap justify-center gap-2">
+          {alphabet.map((letter, index) => (
+            <li key = {index}>
+              <div 
+              onClick = {() => getSelectedLetter(letter)}
+              className = "bg-slate-500 py-2 mx-2 cursor-pointer rounded-xl w-16 flex justify-center hover:brightness-75 transition">
+              <span 
+              className = "py-2 mx-2 text-lg text-slate-50">{letter}</span>
+            </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className = "flex justify-center items-center my-4">
+        <ul>
+        {filteredAuthors.length > 0 ?
+         filteredAuthors.map((author, index) => (
+          <li className = "py-1 font-medium italic cursor-pointer hover:underline"
+          key = {index}>{author}</li>
+        )) : (
+          <p>
+          </p>
+        )}
+        </ul>
       </div>
       {/* <div className = "my-5">
         <h2 className = "text-center my-3">A random Poetry for your pleasure.</h2>
@@ -90,30 +116,9 @@ function App() {
         )
         }
       </div> */}
-
-      <div>
-        <h2 className = "text-center my-2">Authors</h2>
-        <ul className = "flex flex-wrap justify-center gap-2">
-          {alphabet.map((letter, index) => (
-            <li key = {index}>
-              <div className = "bg-slate-500 py-2 mx-2 cursor-pointer rounded-xl w-16 flex justify-center hover:brightness-75 transition">
-              <span 
-              onClick = {() => getSelectedLetter(letter)}
-              className = "py-2 mx-2 text-lg text-slate-50">{letter}</span>
-            </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div>
-        <ul>
-        {filteredAuthors && filteredAuthors.map((author, index) => (
-          <li key = {index}>{author}</li>
-        ))}
-        </ul>
-      </div>
     </div>
+
+    
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,28 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import{
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"; 
+import { Header } from './components/Header.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header/>,
+
+    children:[
+      {
+        path: '/',
+        element: <App/>
+      },
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
